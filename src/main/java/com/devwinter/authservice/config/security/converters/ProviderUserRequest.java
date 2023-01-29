177b2bc73bdfa7fea906b2a236c5e31e542ca936
donghun93 <alswn4516@gmail.com>
2023-01-29 17:32:50 +0900

    Feat: "추가 공용 인증 처리 사용자 Request"
    OAuth 및 Form 로그인 공용 인증 처리 사용자 Request 추가
    
    Resolvers: #1

---
+++ src/main/java/com/devwinter/authservice/config/security/converters/ProviderUserRequest.java
@@ -0,0 +1,16 @@
+package com.devwinter.authservice.config.security.converters;
+
+import com.devwinter.authservice.domain.User;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+public record ProviderUserRequest(ClientRegistration clientRegistration, OAuth2User oAuth2User, User user) {
+
+    public ProviderUserRequest(ClientRegistration clientRegistration, OAuth2User oAuth2User) {
+        this(clientRegistration, oAuth2User, null);
+    }
+
+    public ProviderUserRequest(User user) {
+        this(null, null, user);
+    }
+}
